945c14a4d3e159867243debe28c7a00fab6a70c9
Parimala <Parimala@Parimala>
2022-11-26 21:02:26 -0800

    jacoco

---
+++ src/test/java/exp12/jacoco/AppTest.java
@@ -0,0 +1,29 @@
+package exp12.jacoco;
+
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+  
+	@Test
+    public void test() {
+
+        App p = new App();
+        assertEquals("Hello srivalli", p.getMessage("srivalli"));
+
+    }
+    @Test
+    public void testNameEmpty() {
+
+        App c = new App();
+        assertEquals("Please provide a name!", c.getMessage(" "));
+
+    }
+}
